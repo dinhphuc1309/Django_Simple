c72fd35acc4bc6257da19d423e34e665114eb538
Dinh Phuc <dinhphuc1309@gmail.com>
2023-12-06 16:01:09 +0700

    done ui login screen

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,5 @@ from myapp import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the myapp index.")
+
+def login(request):
+    return render(request, 'login/index.html')
